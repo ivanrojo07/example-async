a6908def567c7da97339e48bf219a2eb97d63b09
Ivan Rojo <ivanrojo07@gmail.com>
2021-10-01 11:01:19 -0500

    initial commit

---
+++ src/utils/fetchData.js
@@ -0,0 +1,28 @@
+let XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+
+const fetchData = (url_api)=>{
+    return new Promise((resolve,reject)=>{
+        const xhttp = new XMLHttpRequest();
+        xhttp.open('GET',url_api,true);
+        xhttp.onreadystatechange = (()=>{
+            if(xhttp.readyState === 4){
+                (xhttp.status === 200)
+                    ? resolve(JSON.parse(xhttp.responseText))
+                    : reject(new Error('Error',url_api))
+                
+                // logica callback
+                // if(xhttp.status === 200){
+                //     callback(null, JSON.parse(xhttp.responseText));
+                // }
+                // else{
+                //     const error = new Error('Error '+url_api);
+                //     return callback(error, null);
+                // }
+            }
+        });
+        xhttp.send()
+    })
+    
+}
+
+module.exports = fetchData;
